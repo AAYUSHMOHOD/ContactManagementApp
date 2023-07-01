98ec206d15542f5516540f9495027848e36a4f13
Aayush <aayushmohod4@gmail.com>
2023-07-01 22:51:50 +0530

    day one

---
+++ routes/contactRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.route("/").get((req,res) => {
+    res.status(200).json({message:"Get all Contacts"});
+});
+
+router.route("/:id").get((req,res) => {
+    res.status(200).json({message: `Get Contact ${req.params.id}`});
+});
+
+router.route("/:id").put((req,res) => {
+    res.status(200).json({message: `Update Contact ${req.params.id}`});
+});
+
+router.route("/").post((req,res) => {
+    res.status(200).json({message:"Create Contact"});
+});
+
+router.route("/:id").delete((req,res) => {
+    res.status(200).json({message:`Delete Contact ${req.params.id}`});
+});
+
+module.exports = router;
